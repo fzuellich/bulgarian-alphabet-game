e08bb47f3b95a870a6d756f955c91d2cb98f4bb5
Glitch (glitch-hello-website) <none>
2024-03-04 21:22:19 +0000

    🏊🏼🕹 Checkpoint ./index.html:65636440/50 ./style.css:65636440/112 ./script.js:65636440/8688

--- script.js
+++ script.js
@@ -2,38 +2,38 @@
   This is your site JavaScript code - you can add interactivity!
 */
 
+// https://en.wikipedia.org/wiki/Bulgarian_alphabet
 const bg = [
   ["А", "/ä/", 'a as in "apart"'],
-["Б", "/b/", 'b as in "bug"'],
-["В", "/v/", 'v as in "vet"'],
-["Г", "/g/", 'g as in "good"'],
-["Д", "/d̪/", 'd as in "dog"'],
-["Е", '/\u025B/', 'e as in "best"'],
-["Ж", "/\u0292/", 's as in "treasure"'],
-["З", "/z/", 'z as in "zoo"'],
-["И", "/i/", 'i as in "machine"'],
-["Й", "/j/", 'y as in "yes"'],
-["К", "/k/", 'k as in "kick"'],
-["Л", "/l/", 'l as in "call"'],
-["М", "/m/", 'm as in "man"'],
-["Н", "/n/", 'n as in "normal"'],
-["О", "/\u0254/", 'o as in "order"'],
-["П", "/p/", 'p as in "pet"'],
-["Р", "/r/", 'r as in spanish "pero"'],
-["С", "/s/", 's as in "sound"'],
-["Т", "/t/", 't as in "stick"'],
-["У", "/u/", 'oo as in "boot"'],
-["Ф", "/f/", 'f as in "food"'],
-["x", "/x/", 'ch as in "loch"'],
-["Ц", "/\u02A6/", 'ts as in "fits"'],
-["Ч", "/\u0074\u0361\u0283/", 'ch as in "chip"'],
-["Ш", "/\u0283/", 'sh as in "shot"'],
-["Щ", "/ʃt/", 'sht as in "shtick"'],
-["Ъ", "/ɤ/", 'u as in "turn"'],
-["Ь", "/j/", 'y as in "canyon"'],
-["Ю", "/ju/", 'yu as in "youth"'],
-["Я", "/ja/", 'ya as in "yarn"']
-
+  ["Б", "/b/", 'b as in "bug"'],
+  ["В", "/v/", 'v as in "vet"'],
+  ["Г", "/g/", 'g as in "good"'],
+  ["Д", "/d̪/", 'd as in "dog"'],
+  ["Е", "/\u025B/", 'e as in "best"'],
+  ["Ж", "/\u0292/", 's as in "treasure"'],
+  ["З", "/z/", 'z as in "zoo"'],
+  ["И", "/i/", 'i as in "machine"'],
+  ["Й", "/j/", 'y as in "yes"'],
+  ["К", "/k/", 'k as in "kick"'],
+  ["Л", "/l/", 'l as in "call"'],
+  ["М", "/m/", 'm as in "man"'],
+  ["Н", "/n/", 'n as in "normal"'],
+  ["О", "/\u0254/", 'o as in "order"'],
+  ["П", "/p/", 'p as in "pet"'],
+  ["Р", "/r/", 'r as in spanish "pero"'],
+  ["С", "/s/", 's as in "sound"'],
+  ["Т", "/t/", 't as in "stick"'],
+  ["У", "/u/", 'oo as in "boot"'],
+  ["Ф", "/f/", 'f as in "food"'],
+  ["x", "/x/", 'ch as in "loch"'],
+  ["Ц", "/\u02A6/", 'ts as in "fits"'],
+  ["Ч", "/\u0074\u0361\u0283/", 'ch as in "chip"'],
+  ["Ш", "/\u0283/", 'sh as in "shot"'],
+  ["Щ", "/ʃt/", 'sht as in "shtick"'],
+  ["Ъ", "/ɤ/", 'u as in "turn"'],
+  ["Ь", "/j/", 'y as in "canyon"'],
+  ["Ю", "/ju/", 'yu as in "youth"'],
+  ["Я", "/ja/", 'ya as in "yarn"'],
 ];
 
 const cyr = (l) => l[0];
@@ -99,11 +99,16 @@ function makeCandidate(label, key) {
 const tableEl = document.getElementById("stage");
 const candidatesEl = document.getElementById("candidates");
 const correctEl = document.getElementById("correct-counter");
+const skipEl = document.getElementById("skip-counter");
+const attemptEl = document.getElementById("attempt-counter");
 
 let targetLabel = cyr;
-let candidateLabel = ipa;
+let candidateLabel = en;
 
 let correct = 0;
+let skip = 0;
+let attempts = 0;
+
 let target = pick(1)[0];
 let candidates = shuffle([...pick(3), target]);
 
@@ -116,9 +121,8 @@ function reset() {
 
 function round() {
   reset();
-  
+
   target = pick(1)[0];
-  console.log({target});
   candidates = shuffle([...pick(3), target]);
   tableEl.appendChild(makeLetterBox(targetLabel(target)));
 
@@ -127,8 +131,10 @@ function round() {
     const candidate = candidates[i];
     candidatesEl.appendChild(makeCandidate(candidateLabel(candidate), key));
   }
-  
+
   correctEl.textContent = correct;
+  skipEl.textContent = skip;
+  attemptEl.textContent = attempts;
 }
 
 round();
@@ -136,8 +142,8 @@ round();
 function match(key) {
   const keyIdx = keys.findIndex((k) => k === key);
   const selectedCandidate = candidates[keyIdx];
-  
-  return (selectedCandidate[0] === target[0]);
+
+  return cyr(selectedCandidate) === cyr(target);
 }
 
 window.addEventListener("keydown", (event) => {
@@ -145,10 +151,19 @@ window.addEventListener("keydown", (event) => {
     return;
   }
 
+  attempts++;
+
+  // SPC skips
+  if (event.keyCode === 32) {
+    skip++;
+    round();
+    return;
+  }
+
   const isGuessCorrect = match(event.key);
   if (isGuessCorrect) {
     correct++;
   }
-  
+
   round();
 });
